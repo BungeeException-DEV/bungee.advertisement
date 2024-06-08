c419731a854e317fbafd53716fa7168713e4c7e7
Cedrik Mutz <67883676+BungeeException-DEV@users.noreply.github.com>
2024-06-08 17:16:44 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BungeeException'
+description 'Mad.V Advertisement Script'
+version '1.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html'
+}
+
+shared_script 'config.lua'
