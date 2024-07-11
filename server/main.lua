77fc63b8c57611b573b9dd7589ed77ab4a68d6a0
Cedrik Mutz <67883676+BungeeException-DEV@users.noreply.github.com>
2024-07-11 19:07:06 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,13 +1,13 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
-RegisterNetEvent('madv.ads:server:sendAd')
-AddEventHandler('madv.ads:server:sendAd', function(adText)
+RegisterNetEvent('bungee.ads:server:sendAd')
+AddEventHandler('bungee.ads:server:sendAd', function(adText)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if Player then
         local playerJob = Player.PlayerData.job.name
         if IsPlayerAllowedToAdvertise(playerJob) then
-            TriggerClientEvent('madv.ads:client:displayAd', -1, adText)
+            TriggerClientEvent('bungee.ads:client:displayAd', -1, adText)
         else
             TriggerClientEvent('QBCore:Notify', src, "You do not have permission to post advertisements.", "error")
         end
